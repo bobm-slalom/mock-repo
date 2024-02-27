26af9b7c9c5421257efec5cc285cef80cfaf74dd
Bob McHenry <bob.mchenry@slalom.com>
2024-02-27 11:50:00 -0800

    init

---
+++ python/false_positive_secret.py
@@ -0,0 +1,5 @@
+not_a_secret = (
+    "Ug_lb0bJ6g6Ckw8D0iqQRdtozxAFhG5mRzLKFqr7k-dzGvhs"  # pragma: allowlist-secret
+)
+
+print(not_a_secret)
---
+++ python/has_secrets.py
@@ -0,0 +1,3 @@
+client_id = "y8auoriO37Tdsjub-GU4DDqk"
+client_secret = "Ug_lb0bJ6g6Ckw8D0iqQRdtozxAFhG5mRzLKFqr7k-dzGvhs"
+print(client_secret)
